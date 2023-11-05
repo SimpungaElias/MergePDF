837b4b39c4f37f80a66b15a3dacf3c1f283a4dab
SIEL <114921056+SimpungaElias@users.noreply.github.com>
2023-11-06 00:58:14 +0300

    Add files via upload

---
+++ mergepdf.py
@@ -0,0 +1,17 @@
+import PyPDF2
+
+# Create a PDF merger object
+pdf_merger = PyPDF2.PdfMerger()
+
+# Add the first PDF file to the merger
+pdf_merger.append("/Users/simpungaelias/Desktop/Radiation Tech.pdf")
+
+# Add the second PDF file to the merger
+pdf_merger.append("/Users/simpungaelias/Desktop/Radiation Technologies.pdf")
+
+# Output merged PDF to a new file
+pdf_merger.write("merged.pdf")
+
+# Close the merger
+pdf_merger.close()
+
